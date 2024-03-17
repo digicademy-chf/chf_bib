1f1154b27dd7cb902ea01504141d52c65703de7f
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-03-17 20:12:36 +0100

    Add reverse label column for BibliographicEntry

---
+++ Configuration/TCA/Overrides/tx_chfbase_domain_model_tag.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+# This file is part of the extension CHF Bib for TYPO3.
+#
+# For the full copyright and license information, please read the
+# LICENSE.txt file that was distributed with this source code.
+
+
+defined('TYPO3') or die();
+
+/**
+ * LabelTag and its properties
+ * 
+ * Extension of a database table and its editing interface in the
+ * TYPO3 backend. This also serves as the basis for the Extbase
+ * domain model. For more information on TCA and its options see
+ * https://docs.typo3.org/m/typo3/reference-tca/main/en-us/.
+ */
+
+// Add column 'asLabelOfBibliographicEntry'
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_chfbase_domain_model_tag',
+    [
+        'asLabelOfBibliographicEntry' => [
+            'exclude' => true,
+            'l10n_mode' => 'exclude',
+            'label' => 'LLL:EXT:chf_bib/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfBibliographicEntry',
+            'description' => 'LLL:EXT:chf_bib/Resources/Private/Language/locallang.xlf:object.labelTag.asLabelOfBibliographicEntry.description',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tx_chfbib_domain_model_bibliographic_entry',
+                'foreign_table_where' => 'AND {#tx_chfbib_domain_model_bibliographic_entry}.{#pid}=###CURRENT_PID###',
+                'MM' => 'tx_chfbib_domain_model_bibliographic_entry_tag_label_mm',
+                'MM_opposite_field' => 'label',
+                'size' => 5,
+                'autoSizeMax' => 10,
+                'fieldControl' => [
+                    'editPopup' => [
+                        'disabled' => false,
+                    ],
+                    'addRecord' => [
+                        'disabled' => false,
+                    ],
+                    'listModule' => [
+                        'disabled' => false,
+                    ],
+                ],
+                'readOnly' => true,
+            ],
+        ],
+    ]
+);
