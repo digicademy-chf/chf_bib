200572217c11d486af4aa87bae729ea837edb1a7
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-06-25 10:16:55 +0200

    Move MM table config to the right extensions

--- ext_tables.sql
+++ ext_tables.sql
@@ -10,6 +10,11 @@ CREATE TABLE tx_chfbase_domain_model_relation (
 
 # Remove when forge.typo3.org/issues/98322 is fixed to auto-generate these fields
 
+CREATE TABLE tx_chfbase_domain_model_relation_bibliographic_entry_bibentry_mm (
+	fieldname varchar(63) DEFAULT '' NOT NULL,
+	tablenames varchar(63) DEFAULT '' NOT NULL
+);
+
 CREATE TABLE tx_chfbib_domain_model_bibliographic_entry_tag_label_mm (
 	fieldname varchar(63) DEFAULT '' NOT NULL,
 	tablenames varchar(63) DEFAULT '' NOT NULL
