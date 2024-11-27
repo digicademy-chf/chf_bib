ab0afa2df90e2459cd762f3748ee351d91ebfb47
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2024-11-28 00:44:21 +0100

    Add cache clearing and annotate controllers

--- Classes/Controller/BibliographyController.php
+++ Classes/Controller/BibliographyController.php
@@ -12,6 +12,7 @@ namespace Digicademy\CHFBib\Controller;
 use Digicademy\CHFBase\Domain\Repository\AbstractResourceRepository;
 use Digicademy\CHFBib\Domain\Model\BibliographicEntry;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Cache\CacheTag;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 defined('TYPO3') or die();
@@ -28,16 +29,43 @@ class BibliographyController extends ActionController
         $this->abstractResourceRepository = $abstractResourceRepository;
     }
 
+    /**
+     * Show bibliographic entry list
+     *
+     * @return ResponseInterface
+     */
     public function indexAction(): ResponseInterface
     {
+        // Get resource
         $resourceIdentifier = $this->settings['resource'];
         $this->view->assign('resource', $this->abstractResourceRepository->findByIdentifier($resourceIdentifier));
+
+        // Set cache tag
+        $this->request->getAttribute('frontend.cache.collector')->addCacheTags(
+            new CacheTag('chf')
+        );
+
+        // Create response
         return $this->htmlResponse();
     }
 
+    /**
+     * Show single bibliographic entry
+     *
+     * @param BibliographicEntry $bibliographicEntry
+     * @return ResponseInterface
+     */
     public function showAction(BibliographicEntry $bibliographicEntry): ResponseInterface
     {
+        // Get bibliographic entry
         $this->view->assign('bibliographicEntry', $bibliographicEntry);
+
+        // Set cache tag
+        $this->request->getAttribute('frontend.cache.collector')->addCacheTags(
+            new CacheTag('chf')
+        );
+
+        // Create response
         return $this->htmlResponse();
     }
 }
